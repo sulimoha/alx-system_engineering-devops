309dd7b7dbd400b2d97f59e0cdd9efb88e80dbd5
Mohamed <sulimmoh@gmail.com>
2023-03-02 17:58:50 -0600

    Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing).

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,11 @@
+# puppet file to automate a 500 error fix
+exec { 'replace phpp with php':
+  command  => 'sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php',
+  provider => shell
+}
+
+#restart apache2
+exec { 'restart apache2 service':
+  command  => 'service apache2 restart',
+  provider => shell
+}
